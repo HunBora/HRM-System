bf87ac40068d3456c4ac274e1bb40d796726caf6
Bora <telalube@gmail.com>
2026-09-02 19:42:16 +0700

    Remove exposed environment variables API

--- src/app/api/env/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'; 
-
-export async function GET() { 
-  return NextResponse.json({ 
-    PRISMA: process.env.POSTGRES_PRISMA_URL, 
-    NON_POOL: process.env.POSTGRES_URL_NON_POOLING 
-  }); 
-}
